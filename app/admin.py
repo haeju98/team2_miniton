34fda8fd50e2c6c6e41d3dac67cb56c76d93f345
haeju98 <tracey1008@naver.com>
2020-06-07 02:15:47 +0900

    4차

--- app/admin.py
+++ app/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post, Product, Comment,UserInfo,Like,Survey,CardNews
+from .models import Post, Product, Comment,UserInfo,Like,Survey,CardNews_model, Community_model,Community_comment
 
 # Register your models here.
 admin.site.register(Post)
@@ -8,4 +8,6 @@ admin.site.register(Comment)
 admin.site.register(UserInfo)
 admin.site.register(Like)
 admin.site.register(Survey)
-admin.site.register(CardNews)
+admin.site.register(CardNews_model)
+admin.site.register(Community_model)
+admin.site.register(Community_comment)
